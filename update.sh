05bd189888e6d1f115402a7aade35f97f5aabaac
xorilog <christophe.boucharlat@surycat.com>
2016-06-27 14:50:58 +0200

    added automatic singing for commits

--- update.sh
+++ update.sh
@@ -4,6 +4,6 @@
 version=$(curl --head https://tdesktop.com/linux/ |grep Location |cut -d '/' -f 5 |cut -d '.' -f 2-4)
 sed -i 's/Telegram\ Version\ [0-9]*\.[0-9]*\.[0-9]*$/\Telegram\ Version\ '$version'/' Dockerfile
 sed -i 's/tsetup.*.tar.xz\ \-O/tsetup.'$version'.tar.xz\ \-O/' Dockerfile
-git commit -am "Telegram version $version"
+git commit -am "Telegram version $version" -S
 git tag -am "Telegram version $version" $version
 git push --follow-tags
