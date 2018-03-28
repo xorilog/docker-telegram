2cd613ba175b5171869c64c5f38858d38cfaaa39
Christophe Boucharlat <christophe.boucharlat@surycat.com>
2018-03-28 14:57:43 +0200

    Telegram version 1.2.15

--- update.sh
+++ update.sh
@@ -2,8 +2,8 @@
 
 # Gather latest version of telegram
 version=$(curl --head https://tdesktop.com/linux/ |grep Location |cut -d '/' -f 5 |cut -d '.' -f 2-4)
-sed -i 's/Telegram\ Version\ [0-9]*\.[0-9]*\.[0-9]*$/\Telegram\ Version\ '$version'/' Dockerfile
-sed -i 's/tsetup.*.tar.xz\ \-O/tsetup.'$version'.tar.xz\ \-O/' Dockerfile
+gsed -i 's/\(Telegram Version \)[0-9]*\.[0-9]*\.[0-9]*$/\1'$version'/' Dockerfile
+gsed -i 's/\(tsetup.\).*\(.tar.xz -O\)/\1'$version'\2/' Dockerfile
 git commit -am "Telegram version $version" -S
 git tag -am "Telegram version $version" $version
 git push --follow-tags
