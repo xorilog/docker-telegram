3ceb983589b57b120d2a9633b0aed9aa7081c488
Christophe Boucharlat <christophe.boucharlat@surycat.com>
2018-03-28 15:35:48 +0200

    removed gnu-sed modification for MacOs

--- update.sh
+++ update.sh
@@ -2,8 +2,8 @@
 
 # Gather latest version of telegram
 version=$(curl --head https://tdesktop.com/linux/ |grep Location |cut -d '/' -f 5 |cut -d '.' -f 2-4)
-gsed -i 's/\(Telegram Version \)[0-9]*\.[0-9]*\.[0-9]*$/\1'$version'/' Dockerfile
-gsed -i 's/\(tsetup.\).*\(.tar.xz -O\)/\1'$version'\2/' Dockerfile
+sed -i 's/\(Telegram Version \)[0-9]*\.[0-9]*\.[0-9]*$/\1'$version'/' Dockerfile
+sed -i 's/\(tsetup.\).*\(.tar.xz -O\)/\1'$version'\2/' Dockerfile
 git commit -am "Telegram version $version" -S
 git tag -am "Telegram version $version" $version
 git push --follow-tags
