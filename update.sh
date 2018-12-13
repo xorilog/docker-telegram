9429c9ae741a208226d2fe0a595e39166103fcf7
Christophe Boucharlat <christophe.boucharlat@gmail.com>
2018-12-13 11:57:21 +0100

    Update telegram urls

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Gather latest version of telegram
-version=$(curl --head https://tdesktop.com/linux/ |grep Location |cut -d '/' -f 5 |cut -d '.' -f 2-4)
+version=$(curl -XGET --head https://telegram.org/dl/desktop/linux |grep Location |cut -d '/' -f 5 |cut -d '.' -f 2-4)
 sed -i 's/\(Telegram Version \)[0-9]*\.[0-9]*\.[0-9]*$/\1'$version'/' Dockerfile
 sed -i 's/\(tsetup.\).*\(.tar.xz -O\)/\1'$version'\2/' Dockerfile
 git commit -am "Telegram version $version" -S
