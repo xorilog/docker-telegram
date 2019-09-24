e4a5607c217e8f7b2350d7e34bd14b1f227bb32e
Christophe Boucharlat <christophe@containo.us>
2019-09-24 10:45:02 +0200

    adding building and pushing to docker.pkg.github.com

--- update.sh
+++ update.sh
@@ -2,8 +2,10 @@
 
 # Gather latest version of telegram
 version=$(curl -XGET --head https://telegram.org/dl/desktop/linux |grep Location |cut -d '/' -f 5 |cut -d '.' -f 2-4)
-sed -i 's/\(Telegram Version \)[0-9]*\.[0-9]*\.[0-9]*$/\1'$version'/' Dockerfile
-sed -i 's/\(tsetup.\).*\(.tar.xz -O\)/\1'$version'\2/' Dockerfile
-git commit -am "Telegram version $version" -S
-git tag -am "Telegram version $version" $version
+sed -i 's/\(Telegram Version \)[0-9]*\.[0-9]*\.[0-9]*$/\1'"${version}"'/' Dockerfile
+sed -i 's/\(tsetup.\).*\(.tar.xz -O\)/\1'"${version}"'\2/' Dockerfile
+git commit -am "Telegram version ${version}" -S
+git tag -am "Telegram version ${version}" "${version}"
 git push --follow-tags
+docker build -t "docker.pkg.github.com/xorilog/docker-telegram/telegram:${version}" .
+docker push "docker.pkg.github.com/xorilog/docker-telegram/telegram:${version}"
