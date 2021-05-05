0c9aac6b9377a6539331ce77478276c887d5ff8c
christophe.boucharlat <christophe.boucharlat@gmail.com>
2021-05-05 16:05:46 +0200

    fix: Location header used to get variable went lowercase

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Gather latest version of telegram
-version=$(curl -XGET --head https://telegram.org/dl/desktop/linux |grep Location |cut -d '/' -f 5 |cut -d '.' -f 2-4)
+version=$(curl -XGET --head https://telegram.org/dl/desktop/linux |grep location |cut -d '/' -f 5 |cut -d '.' -f 2-4)
 sed -i 's/\(Telegram Version \)[0-9]*\.[0-9]*\.[0-9]*$/\1'"${version}"'/' Dockerfile
 sed -i 's/\(tsetup.\).*\(.tar.xz -O\)/\1'"${version}"'\2/' Dockerfile
 
